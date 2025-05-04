23f818bfb6db025c1e4446e8e8c8fab9c366c106
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2025-05-04 18:59:22 +0530

    Update admin_handler.py

--- admin_handler.py
+++ admin_handler.py
@@ -2,7 +2,7 @@ import logging
 from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler
 from chat_data_handler import load_chat_data, get_served_chats, get_served_users
-from telegram.error import TimedOut, NetworkError, RetryAfter, BadRequest, Unauthorized
+from telegram.error import TimedOut, NetworkError, RetryAfter, BadRequest, TelegramError
 
 logger = logging.getLogger(__name__)
 
